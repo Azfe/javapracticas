cae4e24bb43c3b049561e901713bbaa4c498d3ba
jomasonu <jomasonu@PC-MINT>
2020-02-27 19:34:43 +0100

    Ejercicio 4 resuelto.

---
+++ JavaPractica01/src/e04_FichaDomino/FichaDomino.java
@@ -0,0 +1,69 @@
+package e04_FichaDomino;
+
+public class FichaDomino {
+	
+	//Atributos
+	int valor_d, valor_i;
+	
+	//Constructores
+	public FichaDomino () {
+	}
+	
+	public FichaDomino (int valor_d,int valor_i) {
+		this.valor_d = valor_d;
+		this.valor_i = valor_i;
+	}
+
+	// Getters y Setters
+	public int getValor_d() {
+		return valor_d;
+	}
+
+	public void setValor_d(int valor_d) {
+		this.valor_d = valor_d;
+	}
+
+	public int getValor_i() {
+		return valor_i;
+	}
+
+	public void setValor_i(int valor_i) {
+		this.valor_i = valor_i;
+	}
+
+		
+	//Métodos de instancia
+	
+	public boolean encaja(FichaDomino ficha) {
+		if ((this.valor_d == ficha.valor_d) || 
+			(this.valor_d == ficha.valor_i) ||
+			(this.valor_i == ficha.valor_d) ||
+			(this.valor_i == ficha.valor_i)) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void voltea() {
+		this.valor_d = this.valor_d + this.valor_i;
+		this.valor_i = this.valor_d - this.valor_i;
+		this.valor_d = this.valor_d - this.valor_i;
+	}
+
+	@Override
+	public String toString() {
+		if ((valor_d == 0) && (valor_i != 0)) {
+			return "[ |" + valor_i + "]";
+		}else if ((valor_d != 0) && (valor_i == 0)) {
+			return "[" + valor_d + "| ]";
+		}else if ((valor_i == 0 ) && (valor_d == 0)) {
+			return "[ | ]";
+		}else {
+		return "[" + valor_d + "|" + valor_i + "]";
+		}
+	}
+	
+	
+	
+}
